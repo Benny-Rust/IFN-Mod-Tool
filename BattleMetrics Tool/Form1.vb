9526181d408ccbedc2bb7914a5a001cb3a3abc50
Benny <Racyst1447@gmail.com>
2022-05-25 05:20:45 +0100

    Repiared Clipboard

--- BattleMetrics Tool/Form1.vb
+++ BattleMetrics Tool/Form1.vb
@@ -10,86 +10,103 @@
     Private Sub MetroButton2_Click(sender As Object, e As EventArgs) Handles MetroButton2.Click
         ''Chaos Theory
         System.Diagnostics.Process.Start("steam://connect/208.103.169.213:28015")
+        Clipboard.SetText("Connect 208.103.169.213:28015")
     End Sub
 
     Private Sub MetroButton3_Click(sender As Object, e As EventArgs) Handles MetroButton3.Click
         ''Vanguard
         System.Diagnostics.Process.Start("steam://connect/208.103.169.213:28016")
+        Clipboard.SetText("Connect 208.103.169.213:28016")
     End Sub
 
     Private Sub MetroButton4_Click(sender As Object, e As EventArgs) Handles MetroButton4.Click
         ''Bastion
         System.Diagnostics.Process.Start("steam://connect/208.103.169.213:28017")
+        Clipboard.SetText("Connect 208.103.169.213:28017")
     End Sub
 
     Private Sub MetroButton5_Click(sender As Object, e As EventArgs) Handles MetroButton5.Click
         ''Legion
         System.Diagnostics.Process.Start("steam://connect/208.103.169.213:28018")
+        Clipboard.SetText("Connect 208.103.169.213:28018")
     End Sub
 
     Private Sub MetroButton6_Click(sender As Object, e As EventArgs) Handles MetroButton6.Click
         ''Vanilla
         System.Diagnostics.Process.Start("steam://connect/208.103.169.24:28029")
+        Clipboard.SetText("Connect 208.103.169.24:28029")
     End Sub
 
     Private Sub MetroButton7_Click(sender As Object, e As EventArgs) Handles MetroButton7.Click
         ''Battlefield
         System.Diagnostics.Process.Start("steam://connect/208.103.169.106:28017")
+        Clipboard.SetText("Connect 208.103.169.106:28017")
     End Sub
 
     Private Sub MetroButton8_Click(sender As Object, e As EventArgs) Handles MetroButton8.Click
         ''Murphys Law
         System.Diagnostics.Process.Start("steam://connect/208.103.169.71:28026")
+        Clipboard.SetText("Connect 208.103.169.71:28026")
     End Sub
 
     Private Sub MetroButton9_Click(sender As Object, e As EventArgs) Handles MetroButton9.Click
         ''Kamikaze
         System.Diagnostics.Process.Start("steam://connect/208.103.169.106:28016")
+        Clipboard.SetText("Connect 208.103.169.106:28016")
     End Sub
 
     Private Sub MetroButton10_Click(sender As Object, e As EventArgs) Handles MetroButton10.Click
         ''Omega
         System.Diagnostics.Process.Start("steam://connect/208.103.169.24:28028")
+        Clipboard.SetText("Connect 208.103.169.24:28028")
     End Sub
 
     Private Sub MetroButton11_Click(sender As Object, e As EventArgs) Handles MetroButton11.Click
         ''Ozark
         System.Diagnostics.Process.Start("steam://connect/208.103.169.25:28015")
+        Clipboard.SetText("Connect 208.103.169.25:28015")
     End Sub
 
     Private Sub MetroButton12_Click(sender As Object, e As EventArgs) Handles MetroButton12.Click
         ''Oblivion
         System.Diagnostics.Process.Start("steam://connect/208.103.169.105:28018")
+        Clipboard.SetText("Connect 208.103.169.105:28018")
     End Sub
 
     Private Sub MetroButton13_Click(sender As Object, e As EventArgs) Handles MetroButton13.Click
         ''Warden
         System.Diagnostics.Process.Start("steam://connect/208.103.169.105:28016")
+        Clipboard.SetText("Connect 208.103.169.105:28016")
     End Sub
 
     Private Sub MetroButton14_Click(sender As Object, e As EventArgs) Handles MetroButton14.Click
         ''Amigos
         System.Diagnostics.Process.Start("steam://connect/208.103.169.105:28017")
+        Clipboard.SetText("Connect 208.103.169.105:28017")
     End Sub
 
     Private Sub MetroButton15_Click(sender As Object, e As EventArgs) Handles MetroButton15.Click
         ''Alpha
         System.Diagnostics.Process.Start("steam://connect/208.103.169.24:28025")
+        Clipboard.SetText("Connect 208.103.169.24:28025")
     End Sub
 
     Private Sub MetroButton16_Click(sender As Object, e As EventArgs) Handles MetroButton16.Click
         ''Bravo
         System.Diagnostics.Process.Start("steam://connect/208.103.169.24:28027")
+        Clipboard.SetText("Connect 208.103.169.24:28027")
     End Sub
 
     Private Sub MetroButton17_Click(sender As Object, e As EventArgs) Handles MetroButton17.Click
         ''Golden Nugget
         System.Diagnostics.Process.Start("steam://connect/208.103.169.106:28015")
+        Clipboard.SetText("Connect 208.103.169.106:28015")
     End Sub
 
     Private Sub MetroButton18_Click(sender As Object, e As EventArgs) Handles MetroButton18.Click
         ''Skyfall
         System.Diagnostics.Process.Start("steam://connect/208.103.169.105:28015")
+        Clipboard.SetText("Connect 208.103.169.105:28015")
     End Sub
 
     Private Sub MetroButton19_Click(sender As Object, e As EventArgs) Handles MetroButton19.Click
